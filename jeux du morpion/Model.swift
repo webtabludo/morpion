cd9d1f87ffa41963aa440781ad0b237c184ee530
webtabludo <ludo@imac-de-ludo.local>
2018-11-25 14:42:59 +0100

    Pb winner/looser à la seconde partie

--- jeux du morpion/Model.swift
+++ jeux du morpion/Model.swift
@@ -7,3 +7,253 @@
 //
 
 import Foundation
+
+struct Model {
+    
+    
+    // Variables
+    var cellUsedO: [EtatCell.PositionCell.RawValue] = []
+    var cellUsedX: [EtatCell.PositionCell.RawValue] = []
+    var cellUsed: [EtatCell.PositionCell.RawValue] = []
+    var used: [EtatCell.PositionCell.RawValue:EtatCell.StatutCell.RawValue] = ["a":"rien","b":"rien","c":"rien","d":"rien","e":"rien","f":"rien","g":"rien","h":"rien","i":"rien"]
+    
+    
+    
+    // Dictionary combinaison victoire
+    
+    var victoireArray : [[(EtatCell.PositionCell.RawValue)]] = [[EtatCell.PositionCell.a.rawValue,EtatCell.PositionCell.b.rawValue,EtatCell.PositionCell.c.rawValue].sorted(),
+                                                                [EtatCell.PositionCell.d.rawValue,EtatCell.PositionCell.e.rawValue,EtatCell.PositionCell.f.rawValue].sorted(),[EtatCell.PositionCell.g.rawValue,EtatCell.PositionCell.h.rawValue,EtatCell.PositionCell.i.rawValue].sorted(),[EtatCell.PositionCell.a.rawValue,EtatCell.PositionCell.d.rawValue,EtatCell.PositionCell.g.rawValue].sorted(),[EtatCell.PositionCell.b.rawValue,EtatCell.PositionCell.e.rawValue,EtatCell.PositionCell.h.rawValue].sorted(),[EtatCell.PositionCell.c.rawValue,EtatCell.PositionCell.f.rawValue,EtatCell.PositionCell.i.rawValue].sorted(),[EtatCell.PositionCell.a.rawValue,EtatCell.PositionCell.e.rawValue,EtatCell.PositionCell.i.rawValue].sorted(),[EtatCell.PositionCell.c.rawValue,EtatCell.PositionCell.e.rawValue,EtatCell.PositionCell.g.rawValue].sorted()]
+    
+    
+    
+    // Dictionary combinaison proche victoire
+    
+    var procheVictoireArray : [[(EtatCell.PositionCell.RawValue)]] = [[EtatCell.PositionCell.a.rawValue,EtatCell.PositionCell.b.rawValue].sorted(),
+                                                                      [EtatCell.PositionCell.a.rawValue,EtatCell.PositionCell.c.rawValue].sorted(),[EtatCell.PositionCell.a.rawValue,EtatCell.PositionCell.d.rawValue].sorted(),[EtatCell.PositionCell.a.rawValue,EtatCell.PositionCell.e.rawValue].sorted(),[EtatCell.PositionCell.a.rawValue,EtatCell.PositionCell.g.rawValue].sorted(),[EtatCell.PositionCell.a.rawValue,EtatCell.PositionCell.i.rawValue].sorted(),[EtatCell.PositionCell.b.rawValue,EtatCell.PositionCell.c.rawValue].sorted(),[EtatCell.PositionCell.b.rawValue,EtatCell.PositionCell.e.rawValue].sorted(),[EtatCell.PositionCell.b.rawValue,EtatCell.PositionCell.h.rawValue].sorted(),[EtatCell.PositionCell.c.rawValue,EtatCell.PositionCell.e.rawValue].sorted(),[EtatCell.PositionCell.c.rawValue,EtatCell.PositionCell.f.rawValue].sorted(),[EtatCell.PositionCell.c.rawValue,EtatCell.PositionCell.g.rawValue].sorted(),[EtatCell.PositionCell.c.rawValue,EtatCell.PositionCell.i.rawValue].sorted(),[EtatCell.PositionCell.d.rawValue,EtatCell.PositionCell.e.rawValue].sorted(),[EtatCell.PositionCell.d.rawValue,EtatCell.PositionCell.f.rawValue].sorted(),[EtatCell.PositionCell.d.rawValue,EtatCell.PositionCell.g.rawValue].sorted(),[EtatCell.PositionCell.e.rawValue,EtatCell.PositionCell.f.rawValue].sorted(),[EtatCell.PositionCell.e.rawValue,EtatCell.PositionCell.g.rawValue].sorted(),[EtatCell.PositionCell.e.rawValue,EtatCell.PositionCell.h.rawValue].sorted(),[EtatCell.PositionCell.e.rawValue,EtatCell.PositionCell.i.rawValue].sorted(),[EtatCell.PositionCell.f.rawValue,EtatCell.PositionCell.i.rawValue].sorted(),[EtatCell.PositionCell.g.rawValue,EtatCell.PositionCell.h.rawValue].sorted(),[EtatCell.PositionCell.g.rawValue,EtatCell.PositionCell.i.rawValue].sorted(),[EtatCell.PositionCell.h.rawValue,EtatCell.PositionCell.i.rawValue].sorted(),]
+    
+    
+    
+    
+    // Enum état case
+    enum EtatCell {
+        
+        enum StatutCell: String {
+            case croix
+            case rond
+            case rien
+        }
+        
+        enum PositionCell: String {
+            case a
+            case b
+            case c
+            case d
+            case e
+            case f
+            case g
+            case h
+            case i
+        }
+        case cell(statut:StatutCell.RawValue, position:PositionCell.RawValue)
+        
+    }
+    
+    
+    
+    // Fonction jouer
+    mutating func jouer(statut: EtatCell.StatutCell.RawValue, position: EtatCell.PositionCell.RawValue) {
+        
+        
+        guard cellUsed.contains(position) == false else {
+            print("\(position) position déja utilisée")
+            return  }
+        
+        if case let EtatCell.cell(stat, posit) = EtatCell.cell(statut: statut, position: position) {
+            cellUsed.append(posit)
+            used.updateValue(stat, forKey: posit)
+            
+        }
+        print("cellX:\(cellUsedX)  cellO:\(cellUsedO)    celltotatl:\(cellUsed)")
+    }
+    
+    
+    
+    // Détermine les cases utilisées Global par X et par O
+    
+    mutating func misaAJourCase() {
+        for (key,value) in used {
+            if value == "croix" && value == "rond" {
+                cellUsed.append(key)
+            }
+        }
+        
+        for (key,value) in used {
+            if value == "croix"
+            {
+                cellUsedX.append(key)
+            }
+            
+        }
+        
+        for (key,value) in used {
+            if value == "rond"
+            {
+                cellUsedO.append(key)
+            }
+        }
+        
+        
+    }
+    
+    
+    
+    // Donne toute les combinaison possibel en fonction ce qui a été joué
+    
+    func combinations<T>(source: [T], takenBy : Int) -> [[T]] {
+        if(source.count == takenBy) {
+            return [source]
+        }
+        
+        if(source.isEmpty) {
+            return []
+        }
+        
+        if(takenBy == 0) {
+            return []
+        }
+        
+        if(takenBy == 1) {
+            return source.map { [$0] }
+        }
+        
+        var result : [[T]] = []
+        
+        let rest = Array(source.suffix(from: 1))
+        let subCombos = combinations(source: rest, takenBy: takenBy - 1)
+        result += subCombos.map { [source[0]] + $0 }
+        result += combinations(source: rest, takenBy: takenBy)
+        return result
+    }
+    
+    
+    // Fonction Test victoire
+    func victoire (cellUsedBy:[(EtatCell.PositionCell.RawValue)]) -> String {
+        var resultat = ""
+        let result = combinations(source: cellUsedBy, takenBy: 3)
+        
+        guard cellUsed.count != 9 else {
+            print("Egalité Fin de partie")
+            resultat = "Egalité Fin de partie"
+            return resultat
+        }
+        for trigramme in result {
+            if victoireArray.contains(trigramme.sorted()) {
+                if cellUsedBy == cellUsedX {
+                    print("victoire : \(cellUsedBy)")
+                    resultat = "Looser"
+                } else if cellUsedBy == cellUsedO {
+                    print("victoire : \(cellUsedBy)")
+                    resultat = "Winner"
+                }
+            }
+        }
+        print("resultat:\(resultat)")
+        return resultat
+    }
+    
+  
+    
+    
+    
+    // Fonction test adversaire proche de la victoire
+    
+    mutating func almostVictoire () -> String {
+        var positString = ""
+        
+        let result = combinations(source: cellUsedO, takenBy: 2)
+        
+        for bigramme in result {
+            if procheVictoireArray.contains(bigramme.sorted()) {
+                
+                for trigramme in victoireArray where trigramme.contains(bigramme[0]) && trigramme.contains(bigramme[1]) {
+                    
+                    let setTri = Set(trigramme)
+                    let caseAJouer =  setTri.subtracting(Set(bigramme.sorted()))
+                    let position = Array(caseAJouer)
+                    
+                    if cellUsed.contains(position[0]) {
+                        
+                        
+                    } else {
+                        
+                        positString = position[0]
+                        jouer(statut: "croix", position: positString)
+                        
+                    }
+                }
+            }
+        }
+        
+        return positString
+    }
+    
+    
+    
+    
+    //Fonction IA
+    mutating func choixIA() -> String {
+        
+        var result = ""
+        result = almostVictoire()
+        
+        if result == ""  {
+            
+            if !cellUsed.contains(EtatCell.PositionCell.e.rawValue) {
+                
+                jouer(statut: "croix", position: EtatCell.PositionCell.e.rawValue)
+                result = "e"
+            } else if !cellUsed.contains(EtatCell.PositionCell.a.rawValue) {
+                
+                jouer(statut: "croix", position: EtatCell.PositionCell.a.rawValue)
+                result = "a"
+            } else if !cellUsed.contains(EtatCell.PositionCell.c.rawValue) {
+                
+                jouer(statut: "croix", position: EtatCell.PositionCell.c.rawValue)
+                result = "c"
+            } else if !cellUsed.contains(EtatCell.PositionCell.g.rawValue) {
+                
+                jouer(statut: "croix", position: EtatCell.PositionCell.g.rawValue)
+                result = "g"
+            } else if !cellUsed.contains(EtatCell.PositionCell.i.rawValue) {
+                
+                jouer(statut: "croix", position: EtatCell.PositionCell.i.rawValue)
+                result = "i"
+            } else if !cellUsed.contains(EtatCell.PositionCell.b.rawValue) {
+                
+                jouer(statut: "croix", position: EtatCell.PositionCell.b.rawValue)
+                result = "b"
+            } else if !cellUsed.contains(EtatCell.PositionCell.d.rawValue) {
+                
+                jouer(statut: "croix", position: EtatCell.PositionCell.d.rawValue)
+                result = "d"
+            } else if !cellUsed.contains(EtatCell.PositionCell.f.rawValue) {
+                
+                jouer(statut: "croix", position: EtatCell.PositionCell.f.rawValue)
+                result = "f"
+            } else if !cellUsed.contains(EtatCell.PositionCell.h.rawValue) {
+                
+                jouer(statut: "croix", position: EtatCell.PositionCell.h.rawValue)
+                result = "h"
+            } else {
+                
+                jouer(statut: "croix", position: EtatCell.PositionCell.e.rawValue)
+                result = "e"
+            }
+        }
+        
+        return result
+    }
+}
+
+
+
--- jeux du morpion/ViewController.swift
+++ jeux du morpion/ViewController.swift
@@ -9,12 +9,212 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var isMyTurn = false
+    var model = Model()
+    var positPlayByIa = ""
+    
+    @IBOutlet weak var posit1: UIButton!
+    @IBOutlet weak var posit2: UIButton!
+    @IBOutlet weak var posit3: UIButton!
+    @IBOutlet weak var posit4: UIButton!
+    @IBOutlet weak var posit5: UIButton!
+    @IBOutlet weak var posit6: UIButton!
+    @IBOutlet weak var posit7: UIButton!
+    @IBOutlet weak var posit8: UIButton!
+    @IBOutlet weak var posit9: UIButton!
+    
+    @IBOutlet weak var myImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        myImageView.isHidden = true
+        enabledButton(status: false)
+        demarrage()
     }
 
+    // Fonction qui démarre la partie
+    
+    func demarrage() {
 
+        let alert = UIAlertController(title: "Morpion", message: "Voulez vous commencer la partie ?", preferredStyle: .alert)
+        
+        let ok = UIAlertAction(title: "Oui", style: .default, handler: { action in
+        print("Vous commencez")
+            self.myImageView.isHidden = false
+            self.enabledButton(status: true)
+
+        })
+        alert.addAction(ok)
+        let cancel = UIAlertAction(title: "Non", style: .default, handler: { action in
+        print("Je commence")
+            self.myImageView.isHidden = false
+            self.automatisme()
+        })
+        alert.addAction(cancel)
+        DispatchQueue.main.async(execute: {
+            self.present(alert, animated: true)
+        })
+    
+    }
+    
+    @IBAction func selectCase(_ sender: UIButton) {
+        sender.setBackgroundImage(UIImage(named: "O"), for: .normal)
+
+            switch sender.tag {
+            case 1:
+                print("la case a est joué")
+                model.jouer(statut: "rond", position: "a")
+                automatisme()
+            case 2:
+                print("la case b est joué")
+                model.jouer(statut: "rond", position: "b")
+                automatisme()
+
+
+            case 3:
+                print("la case c est joué")
+                model.jouer(statut: "rond", position: "c")
+                automatisme()
+
+
+            case 4:
+                print("la case d est joué")
+                model.jouer(statut: "rond", position: "d")
+                automatisme()
+
+
+            case 5:
+                print("la case e est joué")
+                model.jouer(statut: "rond", position: "e")
+                automatisme()
+
+
+            case 6:
+                print("la case f est joué")
+                model.jouer(statut: "rond", position: "f")
+                automatisme()
+
+
+            case 7:
+                print("la case g est joué")
+                model.jouer(statut: "rond", position: "g")
+                automatisme()
+
+
+            case 8:
+                print("la case h est joué")
+                model.jouer(statut: "rond", position: "h")
+                automatisme()
+
+
+            case 9:
+                print("la case i est joué")
+                model.jouer(statut: "rond", position: "i")
+                automatisme()
+
+            default :
+                print("Sélection Impossible")
+            }
+
+        
+    }
+    
+    
+    func automatisme () {
+        enabledButton(status: false)
+        model.misaAJourCase()
+        if model.victoire(cellUsedBy: model.cellUsedO) != "" {
+            alerte(winner: model.victoire(cellUsedBy: model.cellUsedO))
+            
+        } else {
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1){
+            self.positPlayByIa = self.model.choixIA()
+            print("positPlayIA:\(self.positPlayByIa)")
+            self.setButtonImageCroix(posit: self.positPlayByIa)
+            self.model.misaAJourCase()
+            if self.model.victoire(cellUsedBy: self.model.cellUsedX) != "" {
+                self.alerte(winner: self.model.victoire(cellUsedBy: self.model.cellUsedX))
+            }
+            self.positPlayByIa = ""
+            self.enabledButton(status: true)
+            }
+        }
+    }
+    
+    func setButtonImageCroix(posit: String){
+        let image = UIImage(named: "x")!
+        switch posit {
+        case "a":
+            self.posit1.setBackgroundImage(image, for: .normal)
+        case "b":
+            self.posit2.setBackgroundImage(image, for: .normal)
+        case "c":
+            self.posit3.setBackgroundImage(image, for: .normal)
+        case "d":
+            self.posit4.setBackgroundImage(image, for: .normal)
+        case "e":
+            self.posit5.setBackgroundImage(image, for: .normal)
+        case "f":
+            self.posit6.setBackgroundImage(image, for: .normal)
+        case "g":
+            self.posit7.setBackgroundImage(image, for: .normal)
+        case "h":
+            self.posit8.setBackgroundImage(image, for: .normal)
+        case "i":
+            self.posit9.setBackgroundImage(image, for: .normal)
+        default:
+            print("lol trop moche les switch case")
+            
+        }
+       
+    }
+    
+    
+  
+    // Fonction popup Alert fin de jeu
+    
+    func alerte(winner: String) {
+        
+        
+        
+        let alert = UIAlertController(title: "Game Over", message: winner, preferredStyle: .alert)
+        let ok = UIAlertAction(title: "Ok", style: .default) { (action) in
+            self.posit1.setBackgroundImage(nil, for: .normal)
+            self.posit2.setBackgroundImage(nil, for: .normal)
+            self.posit3.setBackgroundImage(nil, for: .normal)
+            self.posit4.setBackgroundImage(nil, for: .normal)
+            self.posit5.setBackgroundImage(nil, for: .normal)
+            self.posit6.setBackgroundImage(nil, for: .normal)
+            self.posit7.setBackgroundImage(nil, for: .normal)
+            self.posit8.setBackgroundImage(nil, for: .normal)
+            self.posit9.setBackgroundImage(nil, for: .normal)
+            
+            self.demarrage()
+            self.model.cellUsed.removeAll()
+            self.model.cellUsedO.removeAll()
+            self.model.cellUsedX.removeAll()
+        }
+        alert.addAction(ok)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
+    
+// Fonction désactiver bouton
+    func enabledButton(status: Bool) {
+        posit1.isEnabled = status
+        posit2.isEnabled = status
+        posit3.isEnabled = status
+        posit4.isEnabled = status
+        posit5.isEnabled = status
+        posit6.isEnabled = status
+        posit7.isEnabled = status
+        posit8.isEnabled = status
+        posit9.isEnabled = status
+    }
+    
+    
 }
 
